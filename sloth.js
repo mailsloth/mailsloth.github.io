21fa081627d5e615315a2d2dc86ef95b3fa384d6
Charlie Daly <the_charlie_daly@hotmail.co.uk>
2017-12-26 00:29:21 +0000

    Added sloth.js

---
+++ sloth.js
@@ -0,0 +1,73 @@
+function mailsoth_submit(elem) {
+	function placeholder(msg) {
+		elem.value = '';
+		elem.placeholder = msg;
+	}
+	
+	var url = 'https://lazymailchimp.azurewebsites.net/api/add'; // 'https://api.mailsloth.net/add';
+	var req = new XMLHttpRequest();
+	var body = JSON.stringify({
+		Id: "2mTVQ0DtNk",
+		Email: elem.value,
+		SourceUri: window.location.href
+	});
+	
+	req.onreadystatechange = () => {
+		if (req.readyState == 4) { // Done
+			if (req.status == 200) { // OK 
+				placeholder('Subscribed!');
+			}
+		}
+	};
+	
+	req.onerror = (e, status, xhr) => {
+		console.log(status);
+		console.log(xhr);
+		console.log(e);
+		placeholder(req.responseText);
+	};
+	
+	req.open('POST', url, true);
+	req.setRequestHeader('Content-Type', 'application/json');
+	req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	req.send(body);
+	
+	placeholder('Sending...');
+}
+
+(function(){ // Install email forms
+	
+	var elements = document.getElementsByClassName("mailsloth-form");
+	for (var i = 0; i < elements.length; i++) {
+		var e = elements[i];
+		var id = 'ms_input_' + i;
+		e.innerHTML = 
+		`<form> \
+			<input id='${id}' name='ms-email' type='email' placeholder='Enter email address'></input> \
+			<button onclick="mailsoth_submit(${id})" class='ms-submit' type='button'>Subscribe</button> \
+			<br /> \
+			<sup>easy mailing lists by <a href='https://mailsloth.net'>mailsloth</a></sup> \
+		</form>`;
+	}
+}())
+
+function download() {
+	var url = 'https://lazymailchimp.azurewebsites.net/api/retrieve'; // 'https://api.mailsloth.net/add';
+	var req = new XMLHttpRequest();
+	var key = 'vpRbPJD4ko';
+	
+	req.onreadystatechange = () => {
+		if (req.readyState == 4) { // Done
+			if (req.status == 200) { // OK 
+				placeholder('Subscribed!');
+			}
+		}
+	};
+	
+	req.open('GET', url, true);
+	req.setRequestHeader('Content-Type', 'application/json');
+	req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	req.send(body);
+	
+	placeholder('Sending...');
+}
